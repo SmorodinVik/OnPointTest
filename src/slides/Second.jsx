db6b51d8b597ec096df3c2affb43f7d30d77d078
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-11-11 22:17:28 +0300

    add scrollbar to second slide

--- src/slides/Second.jsx
+++ src/slides/Second.jsx
@@ -14,9 +14,25 @@ const Second = () => (
           convallis aenean et. Est lorem ipsum dolor sit amet consectetur adipiscing. Urna nunc id 
           cursus metus aliquam eleifend mi in nulla. Lacus suspendisse faucibus interdum posuere lorem 
           ipsum dolor. Libero nunc consequat interdum varius sit amet mattis. Lorem sed risus ultricies 
-          tristique.
+          tristique. Aliquet porttitor lacus luctus accumsan tortor. A erat nam at lectus urna duis 
+          convallis convallis tellus. Augue interdum velit euismod in pellentesque massa placerat. 
+          Elementum integer enim neque volutpat. Pulvinar neque laoreet suspendisse interdum. Sed 
+          viverra tellus in hac habitasse platea. Tincidunt id aliquet risus feugiat in ante. Nec sagittis 
+          aliquam malesuada bibendum arcu. Cursus eget nunc scelerisque viverra mauris in. Fusce ut 
+          placerat orci nulla pellentesque dignissim. Auctor neque vitae tempus quam pellentesque nec nam 
+          aliquam sem. Turpis tincidunt id aliquet risus feugiat in ante metus dictum. Nunc congue nisi 
+          vitae suscipit tellus mauris a. Eget magna fermentum iaculis eu non diam. Dictum fusce ut placerat 
+          orci nulla pellentesque dignissim enim. Aliquet enim tortor at auctor urna nunc id cursus. Dolor 
+          magna eget est lorem. Neque egestas congue quisque egestas diam in arcu cursus. Ut sem viverra 
+          aliquet eget sit amet. Nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut. 
+          Adipiscing bibendum est ultricies integer quis auctor. Dui nunc mattis enim ut tellus elementum. 
+          Pharetra sit amet aliquam id diam maecenas ultricies. Odio ut enim blandit volutpat maecenas 
+          volutpat. In cursus turpis massa tincidunt dui ut. Vel pretium lectus quam id leo in. Quam viverra 
+          orci sagittis eu. Cursus in hac habitasse platea dictumst quisque sagittis purus. Sem fringilla 
+          ut morbi tincidunt.
         </p>
       </div>
+      <div className="background-box" />
     </div>
   </div>
 );
